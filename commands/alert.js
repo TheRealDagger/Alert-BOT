1fbeacaa887db17b468a77d22d9036a9bb9b6fed
DevCold211 <38896652+DevCold211@users.noreply.github.com>
2018-06-09 21:55:37 -0400

    Update alert.js

--- commands/alert.js
+++ commands/alert.js
@@ -1,1 +1,10 @@
+// Alerts when dagdan comes ONLINE hacker protection
+const Discord = require('discord.js');
 
+module.exports.run = async (bot, message, args) => {
+
+}
+
+module.exports.help = {
+  name: 'alert'
+}
