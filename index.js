a0ded403e27a4a7e9bb879bac0fe87b9443f1301
DevCold211 <38896652+DevCold211@users.noreply.github.com>
2018-06-09 22:37:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
+const bot = new Discord.Client({
+	disableEveryone: true
+});
 
 
 const bot = new Discord.Client({
