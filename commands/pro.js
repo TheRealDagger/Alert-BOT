8441600325385bf1da9538453ab2b2b2187e3f64
DevCold211 <38896652+DevCold211@users.noreply.github.com>
2018-06-09 22:00:09 -0400

    Update pro.js

--- commands/pro.js
+++ commands/pro.js
@@ -1,1 +1,12 @@
+// Shows everything about it only works for the owners of this bot!
+const Discord = require('discord.js');
+
+
+module.exports.run = async (bot, message, args) => {
+
+}
+
+module.exports.help = {
+  name: 'pro'
+}
 
